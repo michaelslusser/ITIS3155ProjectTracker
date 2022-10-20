2aa59e3382cde941203f8578c66441d03c6ef912
Michael Slusser <mslusser@uncc.edu>
2022-10-20 14:04:38 -0400

    updated code with Flask functionality

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 # import modules
 import os
+from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+app.static_folder = './static'
+app.template_folder = './templates'
 
 # routing
 
+# GET / - show the user the landing page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# start server
+# start server at http://127.0.0.1:5000
+app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
