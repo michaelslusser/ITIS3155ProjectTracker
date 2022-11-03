dc9e13e52d17bb46688dd29b3d8863f388ce240a
Michael Slusser <mslusser@uncc.edu>
2022-11-03 13:26:40 -0400

    updated app to use SQLAlchemy

--- app.py
+++ app.py
@@ -2,10 +2,18 @@
 import os
 from flask import Flask
 from flask import render_template
+from database import db
 
 app = Flask(__name__)
 app.static_folder = './static'
 app.template_folder = './templates'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+# initialize the database and bind it to flask
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 # routing
 
